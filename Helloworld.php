43953db1063e142e5dc0eea1a134009e2a758851
unknown <jackyin1996@gmail.com>
2018-04-12 20:55:30 +0800

    hello world.php

---
+++ Helloworld.php
@@ -0,0 +1,40 @@
+<?php 
+/**
+ * 
+ */
+ class owner
+ {
+ 	private $firstname;
+ 	private$lastname;
+ 	private $address;
+ 	private $town;
+ 	private $postcode;
+ 	private $phone;
+ 	private $note
+ 	
+ 	public function setFirstname[$first]{
+ 		$this -> firstname = $first
+ 	}
+ 	public function getFirstname[]{
+ 		return $this -> firstname;
+ 	}
+
+ 		 	public function setlastname[$last]{
+ 			$this -> lastname = $last
+ 		}
+ 			public function getlastname[]{
+ 			return $this -> lastname;
+ 		}
+ 			 		 	public function setaddress[$address]{
+ 						$this -> address = $address
+ 						}
+ 						public function getaddress[]{
+ 						return $this -> address;
+ 						}
+ 	 	public function settown[$town]{
+ 		$this -> town = $town
+ 		}
+ 		public function gettown[]{
+ 		return $this -> town;
+ 		}
+ } ?>
